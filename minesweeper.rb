798331293fc6c32493f854989d5e4402ceb55c78
katarina <golbang.k@gmail.com>
2015-10-27 19:12:01 +0100

    green, ready for refactoring

--- minesweeper.rb
+++ minesweeper.rb
@@ -5,6 +5,66 @@ end
 
 class Board
 
+  class BoardProcessor
+    attr_reader :input
+    attr_reader :output
+
+    def initialize(input)
+      @input = input
+      @output = []
+    end
+
+    def process
+      process_rows
+    end
+
+    def process_rows
+      input.each_with_index { |row, row_idx| process_row row, row_idx }
+    end
+
+    def process_row(row, row_idx)
+      row.each_char.with_index { |char,col_idx| process_cell char, col_idx, row_idx }
+    end
+
+    def process_cell(value, x, y)
+      if x == 0
+        @output << ''
+      end
+      processed_row = @output.last
+      if value == ' '
+        count = detect_count x, y
+        processed_row << (count>0 ? count.to_s : ' ')
+      else
+        processed_row << value
+      end
+    end
+
+    def detect_count(x, y)
+      count = 0
+      [[-1,-1], [0,-1], [1,-1], [1,0], [1,1], [0,1], [-1,1], [-1,0]].each do |operands|
+        neighbour_x = x + operands[0]
+        neighbour_y = y + operands[1]
+        next if !in_range?(neighbour_x, neighbour_y)
+        if input[neighbour_y][neighbour_x] == '*'
+          count += 1
+        end
+      end
+      count
+    end
+
+    def in_range?(x, y)
+      x >= 0 && x < width && y >= 0 && y < height
+    end
+
+    def width
+      input.first.length
+    end
+
+    def height
+      input.length
+    end
+  end
+
   class BoardValidator
 
     attr_reader :input
@@ -30,20 +90,21 @@ class Board
     end
 
     def validate_row(row, row_idx)
-      row.each_char.with_index { |char,col_idx| validate_value char, col_idx, row_idx }
+      row.each_char.with_index { |char,col_idx| validate_cell char, col_idx, row_idx }
     end
 
-    def validate_value(value, x, y)
+    def validate_cell(value, x, y)
       if y == 0 || y == height-1
         if x == 0 || x == width-1
           raise ValueError.new("+ expected on position [#{x}, #{y}]") if value != '+'
-        end
-        if x > 0 && x < width-1
+        elsif x > 0 && x < width-1
           raise ValueError.new("- expected on position [#{x}, #{y}]") if value != '-'
         end
       else
         if x == 0 || x == width-1
           raise ValueError.new("| expected on position [#{x}, #{y}]") if value != '|'
+        else
+          raise ValueError.new("#{value} not expected on position [#{x}, #{y}]") if value != ' ' && value != '*'
         end
       end
     end
@@ -66,6 +127,11 @@ class Board
   def self.parse(input)
     v = BoardValidator.new input
     v.validate
+
+    p = BoardProcessor.new input
+    p.process
+
+    p.output
   end
 
 end
--- minesweeper_test.rb
+++ minesweeper_test.rb
@@ -11,7 +11,6 @@ class MinesweeperTest < Minitest::Test
   end
 
   def test_transform2
-    skip
     inp = ['+-----+', '| * * |', '|     |', '|   * |', '|  * *|',
            '| * * |', '+-----+']
     out = ['+-----+', '|1*2*1|', '|11322|', '| 12*2|', '|12*4*|',
@@ -20,48 +19,41 @@ class MinesweeperTest < Minitest::Test
   end
 
   def test_transform3
-    skip
     inp = ['+-----+', '| * * |', '+-----+']
     out = ['+-----+', '|1*2*1|', '+-----+']
     assert_equal out, Board.transform(inp)
   end
 
   def test_transform4
-    skip
     inp = ['+-+', '|*|', '| |', '|*|', '| |', '| |', '+-+']
     out = ['+-+', '|*|', '|2|', '|*|', '|1|', '| |', '+-+']
     assert_equal out, Board.transform(inp)
   end
 
   def test_transform5
-    skip
     inp = ['+-+', '|*|', '+-+']
     out = ['+-+', '|*|', '+-+']
     assert_equal out, Board.transform(inp)
   end
   def test_transform6
-    skip
     inp = ['+--+', '|**|', '|**|', '+--+']
     out = ['+--+', '|**|', '|**|', '+--+']
     assert_equal out, Board.transform(inp)
   end
 
   def test_transform7
-    skip
     inp = ['+--+', '|**|', '|**|', '+--+']
     out = ['+--+', '|**|', '|**|', '+--+']
     assert_equal out, Board.transform(inp)
   end
 
   def test_transform8
-    skip
     inp = ['+---+', '|***|', '|* *|', '|***|', '+---+']
     out = ['+---+', '|***|', '|*8*|', '|***|', '+---+']
     assert_equal out, Board.transform(inp)
   end
 
   def test_transform9
-    skip
     inp = ['+-----+', '|     |', '|   * |', '|     |', '|     |',
            '| *   |', '+-----+']
     out = ['+-----+', '|  111|', '|  1*1|', '|  111|', '|111  |',
@@ -70,7 +62,6 @@ class MinesweeperTest < Minitest::Test
   end
 
   def test_different_len
-    skip
     inp = ['+-+', '| |', '|*  |', '|  |', '+-+']
     assert_raises(ValueError) do
       Board.transform(inp)
@@ -78,14 +69,12 @@ class MinesweeperTest < Minitest::Test
   end
 
   def test_faulty_border
-    skip
     inp = ['+-----+', '*   * |', '+-- --+']
     assert_raises(ValueError) do
       Board.transform(inp)
     end
   end
   def test_invalid_char
-    skip
     inp = ['+-----+', '|X  * |', '+-----+']
     assert_raises(ValueError) do
       Board.transform(inp)
