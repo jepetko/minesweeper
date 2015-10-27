acceed88d9b90fec3826bbbca3da6d70270f6b2d
katarina <golbang.k@gmail.com>
2015-10-27 19:43:58 +0100

    refactoring to parser and visitors

--- minesweeper.rb
+++ minesweeper.rb
@@ -1,137 +1,130 @@
-require 'pry'
-
 class ValueError < Exception
 end
 
 class Board
 
-  class BoardProcessor
-    attr_reader :input
-    attr_reader :output
+  module Visitors
+    class Visitor
+      attr_reader :input
 
-    def initialize(input)
-      @input = input
-      @output = []
-    end
-
-    def process
-      process_rows
-    end
-
-    def process_rows
-      input.each_with_index { |row, row_idx| process_row row, row_idx }
-    end
-
-    def process_row(row, row_idx)
-      row.each_char.with_index { |char,col_idx| process_cell char, col_idx, row_idx }
-    end
-
-    def process_cell(value, x, y)
-      if x == 0
-        @output << ''
+      def initialize(input)
+        @input = input
       end
-      processed_row = @output.last
-      if value == ' '
-        count = detect_count x, y
-        processed_row << (count>0 ? count.to_s : ' ')
-      else
-        processed_row << value
+
+      def in_range?(x, y)
+        x >= 0 && x < width && y >= 0 && y < height
+      end
+
+      def width
+        input.first.length
+      end
+
+      def height
+        input.length
       end
     end
 
-    def detect_count(x, y)
-      count = 0
-      [[-1,-1], [0,-1], [1,-1], [1,0], [1,1], [0,1], [-1,1], [-1,0]].each do |operands|
-        neighbour_x = x + operands[0]
-        neighbour_y = y + operands[1]
-        next if !in_range?(neighbour_x, neighbour_y)
-        if input[neighbour_y][neighbour_x] == '*'
-          count += 1
+    class ValidationVisitor < Visitor
+
+      def initialize(input)
+        super input
+      end
+
+      def visit(value, x, y)
+        if y == 0 || y == height-1
+          if x == 0 || x == width-1
+            raise ValueError.new("+ expected on position [#{x}, #{y}]") if value != '+'
+          elsif x > 0 && x < width-1
+            raise ValueError.new("- expected on position [#{x}, #{y}]") if value != '-'
+          end
+        else
+          if x == 0 || x == width-1
+            raise ValueError.new("| expected on position [#{x}, #{y}]") if value != '|'
+          else
+            raise ValueError.new("#{value} not expected on position [#{x}, #{y}]") if value != ' ' && value != '*'
+          end
         end
       end
-      count
     end
 
-    def in_range?(x, y)
-      x >= 0 && x < width && y >= 0 && y < height
-    end
+    class CountExposureVisitor < Visitor
+      attr_reader :output
 
-    def width
-      input.first.length
-    end
+      def initialize(input)
+        super input
+        @output = []
+      end
 
-    def height
-      input.length
+      def visit(value, x, y)
+        @output << '' if x == 0
+        processed_row = @output.last
+        if value == ' '
+          count = detect_count x, y
+          processed_row << (count>0 ? count.to_s : ' ')
+        else
+          processed_row << value
+        end
+      end
+
+      private
+
+      def detect_count(x, y)
+        count = 0
+        [[-1,-1], [0,-1], [1,-1], [1,0], [1,1], [0,1], [-1,1], [-1,0]].each do |operands|
+          neighbour_x = x + operands[0]
+          neighbour_y = y + operands[1]
+          next if !in_range?(neighbour_x, neighbour_y)
+          count += 1 if input[neighbour_y][neighbour_x] == '*'
+        end
+        count
+      end
     end
   end
 
-  class BoardValidator
+  class BoardParser
 
     attr_reader :input
+    attr_reader :cell_visitors
 
     def initialize(input)
       @input = input
+      @cell_visitors = []
     end
 
-    def validate
-      validate_width
-      validate_rows
+    def <<(visitor)
+      @cell_visitors << visitor
     end
 
-    def validate_width
-      return true if !input || input.empty?
+    def parse
+      check_width
+      parse_rows
+    end
+
+    def check_width
+      return if !input || input.empty?
       length = input[0].length
-      proc = Proc.new { |el| return if el.length != length }
-      input.each &proc
+      input.each { |el| raise ValueError.new('invalid board') if el.length != length }
     end
 
-    def validate_rows
-      input.each_with_index { |row, row_idx| validate_row row, row_idx }
+    def parse_rows
+      input.each_with_index { |row, row_idx| parse_row row, row_idx }
     end
 
-    def validate_row(row, row_idx)
-      row.each_char.with_index { |char,col_idx| validate_cell char, col_idx, row_idx }
-    end
-
-    def validate_cell(value, x, y)
-      if y == 0 || y == height-1
-        if x == 0 || x == width-1
-          raise ValueError.new("+ expected on position [#{x}, #{y}]") if value != '+'
-        elsif x > 0 && x < width-1
-          raise ValueError.new("- expected on position [#{x}, #{y}]") if value != '-'
-        end
-      else
-        if x == 0 || x == width-1
-          raise ValueError.new("| expected on position [#{x}, #{y}]") if value != '|'
-        else
-          raise ValueError.new("#{value} not expected on position [#{x}, #{y}]") if value != ' ' && value != '*'
-        end
+    def parse_row(row, row_idx)
+      row.each_char.with_index do |char,col_idx|
+        @cell_visitors.each { |visitor| visitor.visit char, col_idx, row_idx }
       end
-    end
-
-    def width
-      input.first.length
-    end
-
-    def height
-      input.length
     end
   end
 
   def self.transform(input)
-    parse input
+    parser = BoardParser.new input
+
+    countExpVisitor = Visitors::CountExposureVisitor.new(input)
+    parser << Visitors::ValidationVisitor.new(input)
+    parser << countExpVisitor
+
+    parser.parse
+    countExpVisitor.output
   end
-
-  private
-
-  def self.parse(input)
-    v = BoardValidator.new input
-    v.validate
-
-    p = BoardProcessor.new input
-    p.process
-
-    p.output
-  end
-
 end
